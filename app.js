3eea4b96669c73b210c81ee9af8ce45c245c01f4
Monica <tamang.monica@gmail.com>
2021-03-18 21:45:56 -0600

    Converted variables into arrays, created a for loop to check user age and subscription status based on 5 values

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
-var age = 16; // User is 16 years old
-var isSubscribed = true; // User is subscribed
+// Setting up two arrays of equal length for the user age and subscription status
+var age = [9, 13, 18, 22, 30];
+var isSubscribed = [true, false, false, true, false];
 
-if (age < 18 && isSubscribed === false) {
-    console.log("The user is younger than 18 and not subscribed");
-} else if (age >= 18 && isSubscribed === false) {
-    console.log("The user is 18 or older and not subscribed");
-} else if (age < 18 && isSubscribed === true) {
-    console.log("The user is younger than 18 and is subscribed");
-} else if (age >= 18 && isSubscribed === true) {
-    console.log("The user is 18 or older and is subscribed");
-} else {
-    console.log("The user age is invalid");
+// Checking through each users' age and subscription status one by one based on the 5 values given, console logging the user subscription status and whether the user is younger than 18 or 18 and older
+for (counter = 0; counter < 5; counter++) {
+    if (age[counter] < 18 && (!isSubscribed[counter])) {
+        console.log("The user is younger than 18 and not subscribed");
+    } else if (age[counter] >= 18 && (!isSubscribed[counter])) {
+        console.log("The user is 18 or older and not subscribed");
+    } else if (age[counter] < 18 && (isSubscribed[counter])) {
+        console.log("The user is younger than 18 and is subscribed");
+    } else if (age[counter] >= 18 && (isSubscribed[counter])) {
+        console.log("The user is 18 or older and is subscribed");
+    } else {
+        console.log("The user age is invalid");
+    }
 }

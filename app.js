a6950fd8951d1fc67c0026db680226624202883a
Monica <tamang.monica@gmail.com>
2021-03-18 22:02:57 -0600

    Made the for loop conditional be dynamic based on the length of the arrays

--- app.js
+++ app.js
@@ -2,8 +2,12 @@
 var age = [9, 13, 18, 22, 30];
 var isSubscribed = [true, false, false, true, false];
 
-// Checking through each users' age and subscription status one by one based on the 5 values given, console logging the user subscription status and whether the user is younger than 18 or 18 and older
-for (counter = 0; counter < 5; counter++) {
+// Storing the length of the age array into a new variable
+// Chosen to use the length of the age array arbitrarily since both arrays are the same length
+var arrayLength = age.length;
+
+// Checking through each users' age and subscription status one by one based on the length of the arrays, console logging the user subscription status and whether the user is younger than 18 or 18 and older
+for (counter = 0; counter < arrayLength; counter++) {
     if (age[counter] < 18 && (!isSubscribed[counter])) {
         console.log("The user is younger than 18 and not subscribed");
     } else if (age[counter] >= 18 && (!isSubscribed[counter])) {
